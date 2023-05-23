a099c989b33d112f3cd09dcc8144d251b0e03484
ochiainobuto <nob55@nifty.com>
2023-05-23 16:34:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def helloIndex():
-    return 'Hello World from Python Flask!'
+@app.route('/')
+def index():
+    return 'hello, world'
+
+if __name__ == '__main__':
+    app.run()
