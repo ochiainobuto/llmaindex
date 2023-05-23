14cb00b1aa9c11c7c574e3368e13d4106b33b216
ochiainobuto <nob55@nifty.com>
2023-05-23 17:12:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import openai
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, send_file
+
 from langchain import OpenAI
 
 
